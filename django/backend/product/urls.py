dbedba9ad0c571cf2f9d638eb69aa4de29bd262e
adamsfey3@gmail.com <adamsfey3@gmail.com>
2022-10-18 10:27:01 +0300

    shoe-url

--- django/backend/product/urls.py
+++ django/backend/product/urls.py
@@ -1,4 +1,4 @@
-from .views import CategoryList,ProductView, CategoryDetail
+from .views import CategoryList,ProductView, CategoryDetail,ShoeView
 
 from django.urls import path ,include
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('category/',CategoryList.as_view()),
     path('category/<str:slug>', CategoryDetail.as_view({'get': 'list'}),name='category_detail'),
     # path('product/<int:pk>',ProductView.as_view()),
-    path('product/',ProductView.as_view())
+    path('product/',ProductView.as_view()),
+    path('shoe/',ShoeView.as_view())
 ]
